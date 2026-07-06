058e11eb66b329688a5080da8841c60c30c9d6da
Yo13038 <13038@holbertonstudents.com>
2026-07-06 16:46:05 +0200

    built a websocket server

---
+++ python_advanced/websockets/echo_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module To implement a server that accepts connections ans echoes back
+"""
+
+
+import asyncio
+import websockets
+
+async def request_client(websocket):
+    """function to handle client request"""
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with websockets.serve(request_client, "localhost", 8765) as server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
