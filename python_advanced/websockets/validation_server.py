ce4c17e1e960fdb4c2dae99992148827c7146ae0
Yo13038 <13038@holbertonstudents.com>
2026-07-07 13:33:53 +0200

    add message validation to a server

---
+++ python_advanced/websockets/validation_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module add basic message validation to a server
+"""
+
+import asyncio
+import websockets
+
+
+async def connection_handler(websocket):
+    
+    async for message in websocket:
+
+        not_empty_message = message.strip()
+
+        if not_empty_message:
+            await websocket.send(f"OK:{message}")
+        else:
+             await websocket.send("ERR:EMPTY")
+
+async def main():
+
+    async with websockets.serve(connection_handler, "localhost", 8765):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
