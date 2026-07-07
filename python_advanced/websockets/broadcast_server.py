f89048b716f23680d9ca879d2f88040d95c8c3df
Yo13038 <13038@holbertonstudents.com>
2026-07-07 14:30:59 +0200

    implement broadcast communication

---
+++ python_advanced/websockets/broadcast_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module that give a response to all from the server
+"""
+
+
+import asyncio
+import websockets
+from websockets.exceptions import ConnectionClosed
+
+clients = set()
+
+
+async def connection_handler(websocket):
+    clients.add(websocket)
+
+    try:
+        async for message in websocket:
+            broadcast = f"B:{message}"
+
+            if clients:
+                await asyncio.gather(
+                    *[client.send(broadcast) for client in clients]
+                )
+
+    except ConnectionClosed:
+        pass
+    finally:
+        clients.discard(websocket)
+
+async def main():
+    """Main entry point to start the broadcast server.
+    """
+    async with websockets.serve(connection_handler, "0.0.0.0", 8765):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+        asyncio.run(main())
+        
