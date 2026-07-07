32e035919bc0e0aead8de22a2baf6fff6f88c554
Yo13038 <13038@holbertonstudents.com>
2026-07-07 10:20:53 +0200

    try number n

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""Module to implement WebSocket client
 """
+Module to implement WebSocket client.
+"""
+
 
 import asyncio
 from websockets import connect
 
 
 async def connect_and_send(host="localhost", port=8765):
-    """Connect to the websocket server and send a message.
-    Accepts host and port arguments passed by the validator.
-    """
-    url = f"ws://{host}:{port}"
-    
-    async with connect(url) as websocket:
+    async with connect("ws://localhost:8765") as websocket:
         await websocket.send("Hello WebSocket")
         response = await websocket.recv()
         print(response)
