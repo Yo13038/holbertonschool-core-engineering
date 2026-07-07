bf1e59d3a8a9b05c5aa7a3440a953b8121dae85c
Yo13038 <13038@holbertonstudents.com>
2026-07-07 15:49:36 +0200

    add pycodestyle

--- python_advanced/websockets/asgi_server.py
+++ python_advanced/websockets/asgi_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module to integrate WebSocket communication into a web-facing Python application.
+Module to integrate WebSocket communication into a web-facing application.
 """
 
 
@@ -10,15 +10,16 @@ from starlette.routing import Route, WebSocketRoute
 
 
 async def homepage(request):
-    
+
     return HTMLResponse("<h1>WebSocket App</h1>")
 
+
 async def websocket_endpoint(websocket):
-    
+
     await websocket.accept()
-    
+
     async for message in websocket.iter_text():
-        await websocket.send_text(message)    
+        await websocket.send_text(message)
 
 app = Starlette(routes=[
     Route("/", homepage),
